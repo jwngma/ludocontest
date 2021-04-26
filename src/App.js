313bce298fcbfd492cdf05e53ab4bda67aaf576b
jwngma basumatary <smkbty@gmail.com>
2021-04-26 10:35:04 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <ScrollToTop />
-        <Header />
-
-        {/* <Switch>
-          <Route exact path="/" component={About} />
-          <Route exact path="/presentation" component={Presentation} />
-          <Route exact path="/features" component={Feature} />
-        </Switch> */}
-      </Router>
+      <ScrollToTop />
+      <Header />
     </div>
   );
 }
