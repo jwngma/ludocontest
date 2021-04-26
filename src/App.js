da6d32fdb9c21e9d6d8e96eb7c7ce3686b5dff2b
jwngma basumatary <smkbty@gmail.com>
2021-04-26 19:20:07 +0530

    firs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Header from "./components/Header";
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <ScrollToTop />
       <Header />
     </div>
   );
