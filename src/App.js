60ba62d25d4e2b7cdc6a009beaf1202bed6de648
jwngma basumatary <smkbty@gmail.com>
2021-04-26 19:48:21 +0530

    firs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Header} />
+          <Route exact path="/features" component={Header} />
+          <Route exact path="/presentation" component={Header} />
+          <Route exact path="/download" component={Header} />
+          <Route exact path="/about" component={Header} />
+
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
+      {/* <Header /> */}
     </div>
   );
 }
