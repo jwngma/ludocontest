266d194da700a97be17bb3665eeeb13215d1ad26
jwngma basumatary <smkbty@gmail.com>
2021-04-26 10:32:39 +0530

    gf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Feature from "./components/Feature";
 import Header from "./components/Header";
-import Presentation from "./components/Presentation";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
